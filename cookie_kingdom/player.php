499038bfcb90429ebfb06f61449123c50de1d69c
offsong <59704429+offsong@users.noreply.github.com>
2023-01-26 14:32:26 +0900

    Add files via upload

---
+++ cookie_kingdom/player.php
@@ -0,0 +1,5 @@
+{"statusCode":200,"message":"Genres","pagination":{"currentPage":null,"nextPage":null,"totalPages":null},"totalQuotes":null,"data":
+
+[
+"Player1","Player2","Player3","Player4","Player5","Player6"
+]}
