61a2c9a46e308b7255310eef136d555e231a64f5
offsong <59704429+offsong@users.noreply.github.com>
2023-01-26 14:48:40 +0900

    Update coupon.php

--- cookie_kingdom/coupon.php
+++ cookie_kingdom/coupon.php
@@ -1,4 +1,4 @@
-{"statusCode":200,"message":"Quotes","pagination":{"currentPage":1,"nextPage":2,"totalPages":43},"totalQuotes":429,
+{"statusCode":200,"message":"Quotes","pagination":{"currentPage":1,"nextPage":2,"totalPages":2},"totalQuotes":2,
 "data":[
 {"quoteGenre":[""],"_id":"63c2847a1516c05306d262ce",
 "quoteText":"My heart leaps up when I beholdA rainbow in the sky:So was it when my life began",
