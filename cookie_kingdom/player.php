f7ed0b538946a4a37dec89427d167d1eee68699b
offsong <59704429+offsong@users.noreply.github.com>
2023-01-27 09:22:35 +0900

    Delete cookie_kingdom directory

--- cookie_kingdom/player.php
+++
@@ -1,5 +0,0 @@
-{"statusCode":200,"message":"Genres","pagination":{"currentPage":null,"nextPage":null,"totalPages":null},"totalQuotes":null,"data":
-
-[
-"Player1","Player2","Player3","Player4","Player5","Player6"
-]}
