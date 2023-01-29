0e6f6c76a3672e1259b2bd22b9c0f15cb8882e39
offsong <59704429+offsong@users.noreply.github.com>
2023-01-29 18:09:47 +0900

    Update moreapp.php

--- moreapp.php
+++ moreapp.php
@@ -1,9 +1,8 @@
 [
 {
-	"id": "1",
 	"name" : "Good Sleep - relaxing sound",
 	"rating": 5.0,
-	"link": "offsong.gigihadid_wallpaper",
-	"imageId": "https://play-lh.googleusercontent.com/w3ENzQrXsrudc2YdYwkxgmlLrZkan11zjbhMY6cScZrFqM5GCiJcVIc0RiBzKe1R_XI=w480-h960-rw"
+	"package_name": "offsong.gigihadid_wallpaper",
+	"image": "https://play-lh.googleusercontent.com/w3ENzQrXsrudc2YdYwkxgmlLrZkan11zjbhMY6cScZrFqM5GCiJcVIc0RiBzKe1R_XI=w480-h960-rw"
 }
 ]
